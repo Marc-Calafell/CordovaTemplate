a24ae89f87ca75257c7fa9e7a6be02b9a60059e5
Marc <mcalafellsmx@gmail.com>
2016-10-13 21:37:02 +0200

    adding third party code

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,3 +3,7 @@ window.$ = window.jquery = require('jquery');
 
 var _ = require('underscore');
 window._ = window.underscore = require('underscore');
+
+$(document).ready(function(){
+    $("#msgid").html(_.capitalize("This is Hello World by JQuery"));
+});
