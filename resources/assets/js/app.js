b4cd49a63e8b27caeed2f5c1ed5b81b5251fe1dc
Marc <mcalafellsmx@gmail.com>
2016-10-13 21:58:28 +0200

    adding third party stylesheets| TODO boto

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,9 @@ window.$ = window.jquery = require('jquery');
 var _ = require('underscore');
 window._ = window.underscore = require('underscore');
 
+require()
+
+
 $(document).ready(function(){
     $("#msgid").html(_.capitalize("This is Hello World by JQuery"));
 });
